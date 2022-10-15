abdc3b0f7689232620fa0fbc6d3ced50c982a982
Developstar <deyeye99@gmail.com>
2022-10-15 07:01:42 -0700

    split string into an array

--- Week-2/level 1- Exercises/level1.js
+++ Week-2/level 1- Exercises/level1.js
@@ -36,5 +36,22 @@ console.log(firstWord)
 let challenge = '30 Days Of JavaScript'
 */
 
-let slicedWord = challenge.slice(3)
-console.log(slicedWord)
+let slicedWord = challenge.slice(3);
+console.log(slicedWord);
+
+
+//Check if the string contains a word Script using includes() method
+/*
+let challenge = '30 Days Of JavaScript'
+*/
+let includesScript = challenge.includes('Script');
+console.log(includesScript)
+
+
+//Split the string into an array using split() method
+/*
+let challenge = '30 Days Of JavaScript'
+*/
+
+let splitString = challenge.split()
+console.log(splitString)
