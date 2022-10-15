8eb842bd14f4d03c7ec64a4c57238bd6b42ce4d2
Developstar <deyeye99@gmail.com>
2022-10-15 05:17:50 -0700

    the length of the string task added

--- Week-2/level 1- Exercises/level1.js
+++ Week-2/level 1- Exercises/level1.js
@@ -1,3 +1,10 @@
 //Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
 
 let challenge = '30 Days Of JavaScript'
+
+//Print the string on the browser console using console.log()
+console.log(challenge)
+
+//Print the length of the string on the browser console using console.log()
+
+console.log(challenge.length)
