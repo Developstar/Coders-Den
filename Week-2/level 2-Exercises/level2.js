586fb2e12431a558b390e8f22a7373401efc1bc4
Developstar <deyeye99@gmail.com>
2022-10-17 14:46:44 +0100

    changed string to number

--- Week-2/level 2-Exercises/level2.js
+++ Week-2/level 2-Exercises/level2.js
@@ -9,3 +9,15 @@ console.log(statement);
 let secondStatement = '"Love is not patronizing and charity isn\'t about pity, it is about love. Charity and love are the same -- with charity you give love, so don\'t just give money but reach out your hand instead."'
 
 console.log(secondStatement);
+
+
+// Check if typeof '10' is exactly equal to 10. If not make it exactly equal
+
+console.log(typeof '10');
+
+// to turn '10' to 10
+let number = '10';
+let num = parseInt(number);
+console.log(num);
+
+console.log(num);
