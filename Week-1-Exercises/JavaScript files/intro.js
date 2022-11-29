fdfb167416d5ef1f2446e12f38211888427bf934
Developstar <deyeye99@gmail.com>
2022-11-29 08:55:13 +0100

    comments in JavaScript

--- Week-1-Exercises/JavaScript files/intro.js
+++ Week-1-Exercises/JavaScript files/intro.js
@@ -1,10 +1,12 @@
-//comments can make code readable
 
-//Welcome to 30DaysofJavaScript
+//comments can make code readable (A single line comment)
+
+//Welcome to 30DaysofJavaScript  (A single line comment)
 
 /*
+(multiline comment)
 comments can make code 
 readable, easy to reuse and
 informative
-*/
+*/    
 
