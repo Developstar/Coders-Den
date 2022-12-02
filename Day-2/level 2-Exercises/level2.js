44c8bbaafe56469bef3d58fcde6a58b85c0023f7
Developstar <deyeye99@gmail.com>
2022-12-02 04:15:30 +0100

    completed day-2, level-2

--- Day-2/level 2-Exercises/level2.js
+++ Day-2/level 2-Exercises/level2.js
@@ -56,18 +56,18 @@ console.log(fullSent.includes('jargon'))
 
 let randNum = Math.random(); // random number generated
 let zeroToHund =  parseInt(randNum * 100) ;
-console.log(zeroToHund);
+console.log(zeroToHund); 
 
 
 // Generate a random number between 50 and 100 inclusively
 let calc = Math.floor(Math.random() * (100 - 50 + 1)) + 50;
-console.log(calc);
+console.log(calc); 
 
 
 // Generate a random number between 0 and 255 inclusively
 let randomNumber = Math.random();
-let zeroToTwoFive = parseInt(randomNumber*255);
-console.log(zeroToTwoFive)
+let zeroToTwoFive = parseInt(randomNumber*256);
+console.log(zeroToTwoFive) 
 
 
 // Access the 'JavaScript' string characters using a random number.
@@ -75,12 +75,30 @@ console.log(zeroToTwoFive)
 let characters = 'JavaScript';
 
 let randomNum = Math.random(); //create random numbers
-let intNum = parseInt(randomNum * 9);
+let intNum = parseInt(randomNum * 10);
 console.log(intNum);
 
 let diffChar = characters[intNum];
 console.log(diffChar);
 
 // Use console.log() and escape characters to print the following pattern.
+// 1 1 1 1 1
+// 2 1 2 4 8
+// 3 1 3 9 27
+// 4 1 4 16 64
+// 5 1 5 25 125
 
- 
+console.log('1\t1\t1\t1\t1\n2\t1\t2\t4\t8\n3\t1\t3\t9\t27\n4\t1\t4\t16\t64\n5\t1\t5\t25\t125')
+
+// Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+let sent = 'You cannot end a sentence with because because because is a conjunction'
+
+// get the position of the first because
+let position = sent.match('because')
+console.log(position)
+
+
+let answer = sent.substr(31, 23)
+
+console.log(answer)
