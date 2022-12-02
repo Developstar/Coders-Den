32574d134dc977e17dace4d22e89fe4287980492
Developstar <deyeye99@gmail.com>
2022-12-02 04:38:51 +0100

    string method match used

---
+++ Day-2/level 3-Exercises/level3.js
@@ -0,0 +1,8 @@
+// 'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+
+let sentence = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+
+let pattern = /love/gi
+let num = sentence.match(pattern)
+console.log(num);
+
