d255d50a91459ace0170885331ae153350d6f866
Developstar <deyeye99@gmail.com>
2022-12-02 05:39:26 +0100

    cleaned up a sentence using .replace method

--- Day-2/level 3-Exercises/level3.js
+++ Day-2/level 3-Exercises/level3.js
@@ -7,3 +7,29 @@ let num = sentence.match(pattern)
 console.log(num);
 let numOfLove = num.length;
 console.log(numOfLove)
+
+
+// Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+let sent = 'You cannot end a sentence with because because because is a conjunction';
+let pattern2 = /because/g
+
+let allBecause = sent.match(pattern2)
+console.log(allBecause)
+
+// number of all because 
+
+let numOfBecause = allBecause.length;
+console.log(numOfBecause)
+
+
+// Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+ const sentenc = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+
+ let cleanUp = sentenc.replace('%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching', 'I am a teacher, and I love teaching. There is nothing as  more rewarding as educating and empowering people. I found teaching more interesting than other jobs. Does this motivate you to be a teacher? This 30DaysOfJavaScript is also the result of love of teaching')
+
+ console.log(cleanUp)
+
+
+
+
