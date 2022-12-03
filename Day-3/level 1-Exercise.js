467caf993c973385649188cc096408e153c31ab6
Developstar <deyeye99@gmail.com>
2022-12-03 21:27:15 +0100

    Initial commit

---
+++ Day-3/level 1-Exercise.js
@@ -0,0 +1,23 @@
+// Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+
+
+let firstName = 'John';
+console.log(typeof firstName);
+
+let lastName = 'Adeyeye';
+console.log(typeof lastName);
+
+let country = 'Nigeria';
+console.log(typeof country);
+
+let city = 'Osogbo';
+console.log(typeof city);
+
+let age = 24;
+console.log(typeof age);
+
+let isMarried = false;
+console.log(typeof isMarried);
+
+let year = 2022;
+console.log(typeof year);
