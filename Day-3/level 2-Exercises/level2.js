376143e9ab2bda50d2c71a546d2ea35c0f73e3a1
Developstar <deyeye99@gmail.com>
2022-12-04 13:38:17 +0100

    calculated the area & circum of a circle

--- Day-3/level 2-Exercises/level2.js
+++ Day-3/level 2-Exercises/level2.js
@@ -24,10 +24,21 @@
 
 // Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
 
-let length = prompt('Enter length');
-let width = prompt('Enter width');
-// Area of rectangle
-let area = length * width
-let perimeter = 2 * (parseInt(length) + parseInt(width));
-alert(area);
-alert(perimeter);
+// let length = prompt('Enter length');
+// let width = prompt('Enter width');
+// // Area of rectangle
+// let area = length * width
+// let perimeter = 2 * (parseInt(length) + parseInt(width));
+// alert(area);
+// alert(perimeter);
+
+// Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
+
+let r = prompt('Entire radius here')
+const pi = 3.14;
+
+let areaOfCircle = pi * r * r;
+alert(`The area of circle = ${areaOfCircle}`)
+
+let circumference = 2 * pi * r;
+alert(`The circumference of your circle = ${circumference}`)
