1ca53461b998872799d573745b8d0b9a10478a4d
Developstar <deyeye99@gmail.com>
2022-12-04 10:38:36 +0100

    calculated area of triangle using Prompt and alert

---
+++ Day-3/level 2-Exercises/level2.js
@@ -0,0 +1,7 @@
+// Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
+
+let b = prompt('enter base  here')
+let h = prompt('enter height here')
+
+let area = 0.5 * b * h;
+alert(`The area of your triangle is ${area}`);
