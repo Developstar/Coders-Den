eefc2dbe056268052d5101366d3180f4bd811afe
Developstar <deyeye99@gmail.com>
2022-12-06 01:56:19 +0100

    If else statement added

--- Day-4/level 1-Exercises/level1.js
+++ Day-4/level 1-Exercises/level1.js
@@ -1,12 +1,25 @@
 // Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
 
-let userAge = prompt('Enter your age')
+// let userAge = prompt('Enter your age')
 
-switch(true){
-    case userAge >= 18 :
-    alert('You are old enough to drive');
-    break;
+// switch(true){
+//     case userAge >= 18 :
+//     alert('You are old enough to drive');
+//     break;
 
-    default:
-        alert(`Sorry you'll have to wait for ${18 - userAge} year(s) before you can drive`);
+//     default:
+//         alert(`Sorry you'll have to wait for ${18 - userAge} year(s) before you can drive`);
+// }
+
+
+// Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
+
+let myAge = 24
+
+let yourAge = prompt('Enter your Age')
+
+if(yourAge > myAge){
+    console.log('you are older than me')
+}else{
+    console.log('I am older than you are')
 }
