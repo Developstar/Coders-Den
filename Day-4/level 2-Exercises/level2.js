84909e57cb531695f090ffc0fda5a3a92138362f
Developstar <deyeye99@gmail.com>
2022-12-06 11:29:03 +0100

    season checker built with switch conditional

--- Day-4/level 2-Exercises/level2.js
+++ Day-4/level 2-Exercises/level2.js
@@ -6,31 +6,61 @@
 // 0-49, F
 
 
-let userScore = prompt('Enter your score here')
-let score = parseInt(userScore)
+// let userScore = prompt('Enter your score here')
+// let score = parseInt(userScore)
 
 
-switch(true){
-    case  score >= 90 :
-    console.log('your grade = A')
-    break
+// switch(true){
+//     case  score >= 90 :
+//     console.log('your grade = A')
+//     break
 
-    case score >= 70:
-    console.log('your grade = B')
-    break
+//     case score >= 70:
+//     console.log('your grade = B')
+//     break
 
-    case score >= 60:
-    console.log('your grade = C')
-    break
+//     case score >= 60:
+//     console.log('your grade = C')
+//     break
 
-    case score >= 50:
-        console.log('your grade = E')
-        break
+//     case score >= 50:
+//         console.log('your grade = E')
+//         break
 
-    case score >= 0:
-        console.log('Your grade = F' )
-        break
+//     case score >= 0:
+//         console.log('Your grade = F' )
+//         break
 
-    default:
-        console.log('Not a number')
+//     default:
+//         console.log('Not a number')
+// }
+
+
+// Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+// September, October or November, the season is Autumn.
+// December, January or February, the season is Winter.
+// March, April or May, the season is Spring
+// June, July or August, the season is Summer
+
+
+let userInput = prompt('What month are we?')
+let month = userInput.toLowerCase()
+
+
+switch(month){
+    case "september"  :
+    case  "october"   :
+    case  "november"  :
+        console.log('the season is Autumn')
+        break;
+    case  "december" :
+    case  "january"  :
+    case  "february" :  
+        console.log('the season is Winter')
+        break;
+    default :
+    console.log('Invalid input')
+    break;
 }
+
+
