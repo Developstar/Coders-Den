e1b1699f4f2682f844178d74a98d56f81b59d7d6
Developstar <deyeye99@gmail.com>
2022-12-06 02:10:31 +0100

    tenary operator added

--- Day-4/level 1-Exercises/level1.js
+++ Day-4/level 1-Exercises/level1.js
@@ -34,8 +34,12 @@ let a = 4
 let b = 3
 
 
-if(a > b){
-    console.log('a is greater than b')
-}else{
-    console.log('a is less than b')
-}
+// if(a > b){
+//     console.log('a is greater than b')
+// }else{
+//     console.log('a is less than b')
+// }
+
+a > b
+? console.log('a is greater than b')
+: console.log('a is less than b')
