07e181b892615243c321ac9b0a878bb8e07e374b
Developstar <deyeye99@gmail.com>
2022-12-10 21:21:02 +0100

    printed elements in an arrayout like a sentence

--- Day-5/level 1-Exercices/level1.js
+++ Day-5/level 1-Exercices/level1.js
@@ -105,3 +105,8 @@ companies[6] = companySeven.toUpperCase();
 console.log(companies)
 
 
+// Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+
+console.log(companies.join())
+
+
