c35cbc4566b59c9406baca1a3fa5eea4df8f8060
Developstar <deyeye99@gmail.com>
2022-12-13 15:58:15 +0100

    added and removed items using unshift, push,splice

--- Day-5/level 2-Exercises/index.js
+++ Day-5/level 2-Exercises/index.js
@@ -17,9 +17,24 @@ let text = 'I love teaching and empowering people. I teach HTML, CSS, JS, React,
 // console.log(text)
 
 // count the number of words in the array
- text = text.split(' ')
- console.log(text)
- let lengthOfText = text.length;
- console.log(lengthOfText)
+//  text = text.split(' ')
+//  console.log(text)
+//  let lengthOfText = text.length;
+//  console.log(lengthOfText)
+
+// In the following shopping cart add, remove, edit items
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+// add 'Meat' in the beginning of your shopping cart if it has not been already added
+shoppingCart.unshift('Meat');
+console.log(shoppingCart)
+// add Sugar at the end of your shopping cart if it has not been already added
+shoppingCart.push('Sugar')
+console.log(shoppingCart)
+// remove 'Honey' if you are allergic to honey
+shoppingCart.splice(4,1)
+console.log(shoppingCart)
+// modify Tea to 'Green Tea'
+shoppingCart.splice(3,1,'Green Tea')
+console.log(shoppingCart)
 
 
