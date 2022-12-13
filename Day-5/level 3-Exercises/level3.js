5d5a940815dd9e8774b9a5a3f2106f7ccc693e46
Developstar <deyeye99@gmail.com>
2022-12-13 18:02:19 +0100

    age array sorted &logged the highest & lowest age

---
+++ Day-5/level 3-Exercises/level3.js
@@ -0,0 +1,16 @@
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+
+// Sort the array and find the min and max age
+ages.sort();
+console.log(ages)
+//minAge
+let minAge = ages[0]
+console.log(minAge)
+//maxAge
+let maxAgeIndex = ages.length - 1
+console.log(maxAgeIndex)
+let maxAge = ages[maxAgeIndex]
+console.log(maxAge)
+
+
+
