2acdabe54b856cb0e838fc3ea749cd6b638f4ffa
Developstar <deyeye99@gmail.com>
2022-12-13 17:19:31 +0100

    used switch statement to check if Sass is included

--- Day-5/level 2-Exercises/web_techs.js
+++ Day-5/level 2-Exercises/web_techs.js
@@ -8,7 +8,25 @@ export const webTechs = [
     'MongoDB'
   ]
 
+  // In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+
+  switch(webTechs){
+    case  webTechs.includes('Sass') :
+    console.log('Sass is a CSS preprocess')
+    break
+
+    default:
+      webTechs.push('Sass')
+  }
+
   console.log(webTechs)
+
+
+
+
+
+
+
 
 export default webTechs;
 
