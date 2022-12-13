8fbe315315d267dacc7fb8be8d44be269a46773e
Developstar <deyeye99@gmail.com>
2022-12-13 16:10:31 +0100

    an if/else written if a stat. is true or not.

--- Day-5/level 2-Exercises/countries.js
+++ Day-5/level 2-Exercises/countries.js
@@ -12,6 +12,25 @@ const countries = [
     'Kenya'
   ]
 
+  // In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+
+  if(countries.includes('Ethiopia') === true){
+    console.log('ETHIOPIA')
+  }else{
+    countries.push('Ethiopia')
+  }
+
+  
+
+
+
+
+
+
+
+
+
+
 
 
   export default countries;
