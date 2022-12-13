00cea1488a5d343fecf61a515ecc4f265a9bc0c1
Developstar <deyeye99@gmail.com>
2022-12-13 12:01:29 +0100

    accessed both countries & web js files in index.js

---
+++ Day-5/level 2-Exercises/web_techs.js
@@ -0,0 +1,14 @@
+export const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ]
+
+  console.log(webTechs)
+
+export default webTechs;
+
