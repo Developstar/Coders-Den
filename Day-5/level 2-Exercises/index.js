28d5ab24f5d5a1d6e4c6f020737d77c2a2478528
Developstar <deyeye99@gmail.com>
2022-12-13 14:28:27 +0100

    removed punctuation from a string, string to array

--- Day-5/level 2-Exercises/index.js
+++ Day-5/level 2-Exercises/index.js
@@ -1,8 +1,20 @@
 // Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
-import countries from './countries.js';
-import webTechs from './web_techs.js';
+// import countries from './countries.js';
+// import webTechs from './web_techs.js';
 
 
-console.log(countries);
-console.log(webTechs)
+// console.log(countries);
+// console.log(webTechs)
+
+// First remove all the punctuations and change the string to array and count the number of words in the array
+let text = 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+//Remove punctuations
+let removedPunctuation = text.replace(/[.\,]/g, "");
+console.log(removedPunctuation)
+text = removedPunctuation
+//change the string to array
+text = text.split(' ,')
+console.log(text)
+
+
 
