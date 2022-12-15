cf15ba1b52992d82d580eaa4e5f7f118dd953869
Developstar <deyeye99@gmail.com>
2022-12-15 06:45:45 +0100

    for, while, and do while loop to print 10 num

---
+++ Day-6/level 1-Exercises/level1.js
@@ -0,0 +1,20 @@
+// Iterate 0 to 10 using for loop, do the same using while and do while loop
+for
+for(let i = 0; i <= 10; i++){
+    console.log(i)
+}
+
+//while
+let i = 0
+while( i <= 10){
+   console.log(i) 
+   i++
+    
+}
+
+//do while
+let i = 0
+do{
+    console.log(i)
+    i++
+}while(i <= 10)
