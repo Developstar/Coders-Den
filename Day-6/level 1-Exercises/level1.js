6ee3a361cb9a0987fa79dad43d14bc0984aa7cf2
Developstar <deyeye99@gmail.com>
2022-12-16 05:46:57 +0100

    loop to iterate from 0 - 100 and print odd numbers

--- Day-6/level 1-Exercises/level1.js
+++ Day-6/level 1-Exercises/level1.js
@@ -76,8 +76,16 @@
 
 // Use for loop to iterate from 0 to 100 and print only even numbers
 
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// Use for loop to iterate from 0 to 100 and print only odd numbers
+
 for (let i = 1; i <= 100; i++) {
-  if (i % 2 === 0) {
+  if (i % 2 !== 0) {
     console.log(i);
   }
 }
