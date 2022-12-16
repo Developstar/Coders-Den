932bf190d70f8533768c5f00a60b0c3b6c5b8b38
Developstar <deyeye99@gmail.com>
2022-12-16 04:58:01 +0100

    for loop to print out an arithmetic calculation

--- Day-6/level 1-Exercises/level1.js
+++ Day-6/level 1-Exercises/level1.js
@@ -28,16 +28,31 @@
 // ######
 // #######
 
-for(let i = 1; i <= 7; i++){
-  let line = '';
-  for(let n = 0; n < i; n++){
-    line += '#'
+// for(let i = 1; i <= 7; i++){
+//   let line = '';
+//   for(let n = 0; n < i; n++){
+//     line += '#'
     
-  }
-    console.log(line)
- }
+//   }
+//     console.log(line)
+//  }
 
 
- 
 
+// Use loop to print the following pattern:
+// 0 x 0 = 0
+// 1 x 1 = 1
+// 2 x 2 = 4
+// 3 x 3 = 9
+// 4 x 4 = 16
+// 5 x 5 = 25
+// 6 x 6 = 36
+// 7 x 7 = 49
+// 8 x 8 = 64
+// 9 x 9 = 81
+// 10 x 10 = 100
+
+for(let i = 0; i <= 10; i++ ){
+    console.log(`${i} x ${i} = ${i * i}`)
+}
 
