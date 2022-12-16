964987ec02f8a67bd8d349858832df2659385a7d
Developstar <deyeye99@gmail.com>
2022-12-16 05:14:21 +0100

    for loop used to print pattern

--- Day-6/level 1-Exercises/level1.js
+++ Day-6/level 1-Exercises/level1.js
@@ -52,7 +52,28 @@
 // 9 x 9 = 81
 // 10 x 10 = 100
 
-for(let i = 0; i <= 10; i++ ){
-    console.log(`${i} x ${i} = ${i * i}`)
-}
+// for(let i = 0; i <= 10; i++ ){
+//     console.log(`${i} x ${i} = ${i * i}`)
+// }
 
+
+
+// Using loop print the following pattern
+
+//  i    i^2   i^3
+//  0    0     0
+//  1    1     1
+//  2    4     8
+//  3    9     27
+//  4    16    64
+//  5    25    125
+//  6    36    216
+//  7    49    343
+//  8    64    512
+//  9    81    729
+//  10   100   1000
+
+console.log(`i  i^2  i^3`)
+for(let i = 0; i <= 10; i++){
+    console.log(`${i}   ${i**2}   ${i**3}`)
+}
