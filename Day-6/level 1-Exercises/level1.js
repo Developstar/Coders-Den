9e52734ee1cc9c90107e140e50b1090c7776429d
Developstar <deyeye99@gmail.com>
2022-12-16 04:32:14 +0100

    a for loop that prints out # in a pyramidlike form

--- Day-6/level 1-Exercises/level1.js
+++ Day-6/level 1-Exercises/level1.js
@@ -1,20 +1,43 @@
 // Iterate 0 to 10 using for loop, do the same using while and do while loop
-for
-for(let i = 0; i <= 10; i++){
-    console.log(i)
-}
+// for
+// for(let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
 
 //while
-let i = 0
-while( i <= 10){
-   console.log(i) 
-   i++
+// let i = 0
+// while( i <= 10){
+//    console.log(i) 
+//    i++
     
-}
+// }
 
 //do while
-let i = 0
-do{
-    console.log(i)
-    i++
-}while(i <= 10)
+// let i = 0
+// do{
+//     console.log(i)
+//     i++
+// }while(i <= 10)
+
+// Write a loop that makes the following pattern using console.log():
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+for(let i = 1; i <= 7; i++){
+  let line = '';
+  for(let n = 0; n < i; n++){
+    line += '#'
+    
+  }
+    console.log(line)
+ }
+
+
+ 
+
+
