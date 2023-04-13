b1136f0006deaa9472515940de902ec61b59fdce
Developstar <deyeye99@gmail.com>
2023-04-13 08:34:02 +0100

    Q: function that  takes fN, lN as a parameter

--- Day-7/level 1-Exercises/level1.js
+++ Day-7/level 1-Exercises/level1.js
@@ -1,7 +1,17 @@
 //Declare a function fullName and it print out your full name.
 
-function fullName(){
-  console.log('Adeyeye Boluwatife') 
+// function fullName(){
+//   console.log('Adeyeye Boluwatife') 
+// }
+
+// fullName()
+
+
+//Declare a function fullName and now it takes firstName, lastName as a parameter and it returns your full - name.
+
+function fullName(firstName, lastName){
+  let Name = `${firstName} ${lastName}`
+  console.log(Name)
 }
 
-fullName()
+fullName('Boluwatife','Adeyeye')
