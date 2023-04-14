caa090a8f3efa7a2e9994f7bb508e64dbf11a9af
Developstar <deyeye99@gmail.com>
2023-04-14 04:38:30 +0100

    function which calculates bmi and Check weight

--- Day-7/level 1-Exercises/level1.js
+++ Day-7/level 1-Exercises/level1.js
@@ -118,3 +118,32 @@ function convertTempToOf(oC){
 }
 
 convertTempToOf(15)
+
+
+//Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2. Write a function which calculates bmi. BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
+
+// The same groups apply to both men and women.
+// Underweight: BMI is less than 18.5
+// Normal weight: BMI is 18.5 to 24.9
+// Overweight: BMI is 25 to 29.9
+// Obese: BMI is 30 or more
+
+
+function bodyMassIndex(weight,height){
+  // Convert height to meters
+  height = height / 100;
+  //calculate BMI
+  mass = weight/(height * height)
+  console.log(mass)
+  if(mass >= 30 ){
+    console.log('Obese' )
+  }else if(mass >= 25 ){
+    console.log('Overweight')
+  }else if(mass >= 18.5){
+    console.log('Normal weigth')
+  }else if(mass<18.5){
+    console.log('Underweight')
+  }
+}
+
+bodyMassIndex(72,185)
