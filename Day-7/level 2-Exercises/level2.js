d8de56a3c0a19b0f1e583f8df0f362ac7466a003
Developstar <deyeye99@gmail.com>
2023-04-15 17:03:29 +0100

    function that takes array as a parameter & prints value

---
+++ Day-7/level 2-Exercises/level2.js
@@ -0,0 +1,12 @@
+// Declare a function name printArray. It takes array as a parameter and it prints out each value of the array.
+let array = [1,2,3,4,5,6]
+
+function printArray(array){
+    for(let i = 0; i<array.length; i++){
+        console.log(array[i])
+    }
+}
+
+printArray(array)
+
+
