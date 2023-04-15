5285ec342cbbb74af3ed3accff120e3dc7d53fcf
Developstar <deyeye99@gmail.com>
2023-04-15 17:50:48 +0100

    Q: Declare a function named swapValues. This function swaps value of x to y

--- Day-7/level 2-Exercises/level2.js
+++ Day-7/level 2-Exercises/level2.js
@@ -18,3 +18,16 @@ console.log(`${day}/${month}/${year} ${hour}:${minute}`)
 }
 
 showDateTime()
+
+
+// Declare a function name swapValues. This function swaps value of x to y.
+// swapValues(3, 4) // x => 4, y=>3
+// swapValues(4, 5) // x = 5, y = 4
+
+function swapValues(x, y){
+    let input1 = x;
+    let input2 = y;
+    console.log(`x => ${y}, y=>${x}`)
+}
+
+swapValues(5,4)
