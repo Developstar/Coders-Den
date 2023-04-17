e80ceffb9f85b56724c044b0650a49b0d6b93950
Developstar <deyeye99@gmail.com>
2023-04-17 16:40:04 +0100

    Add name, legs, color, age and bark properties to the dog object. The bark property is a method which return woof woof

--- Day-8/level 1-Exercises/level1.js
+++ Day-8/level 1-Exercises/level1.js
@@ -1,4 +1,24 @@
 //Create an empty object called dog
 let dog = {}
 
+//Print  the dog object on the console
+console.log(dog)
+
+//Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof
+dog.name = 'Bingo'
+dog.legs = 4
+dog.color = 'Brown'
+dog.age = 5
+dog.bark = function(){
+  return 'woof woof'
+}
+
+console.log(dog.bark())
+
+
+
+
+
+
+
 
