0fbcafacbc99a62c66ce6969b74ef01487596f9a
Developstar <deyeye99@gmail.com>
2023-08-08 03:35:41 +0100

    get element using querySelector

---
+++ Day-21/level 1-Exercises/index.js
@@ -0,0 +1,5 @@
+// Create an index.html file and put four p elements as above: Get the first paragraph by using document.querySelector(tagname) and tag name
+
+let firstElement = document.querySelector("p");
+
+
